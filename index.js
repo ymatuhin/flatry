2e7984148f628d561f7f1abca5dfcda0ee237833
Yury Matuhin <ym@ymatuhin.ru>
2018-03-30 15:06:43 +0300

    Fix windows xp webpack export

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const isPromise = require('is-promise');
+var isPromise = require('is-promise');
 
 function flatrySync(fn) {
   try {
@@ -8,19 +8,17 @@ function flatrySync(fn) {
   }
 }
 
-function flatry(fn) {
+module.exports = function flatry(fn) {
   if (typeof fn !== 'function' && !isPromise(fn)) {
     throw new Error('Argument must be a function or Promise');
   }
 
-  var successFn = function(value) {
+  var successFn = function (value) {
     return [null, value];
   };
-  var errorFn = function(err) {
+  var errorFn = function (err) {
     return [err, null];
   };
 
   return isPromise(fn) ? fn.then(successFn, errorFn) : flatrySync(fn);
 }
-
-module.exports = flatry;
