0943c52b0f92e2bd1cba4797d949bb2de5ca741c
ymatuhin <ym@ymatuhin.ru>
2024-01-13 22:30:49 +0300

    release 1.0.15

--- cjs/index.d.ts
+++ cjs/index.d.ts
@@ -1,2 +1,2 @@
-export default function flatry<T>(promise: Promise<T>): Promise<[unknown, never] | [null, T]>;
+export default function flatry<T>(promise: PromiseLike<T>): Promise<[unknown, never] | [null, T]>;
 export default function flatry<T>(fn: () => T): [unknown, never] | [null, T];
